1df334b5b95998f3eebf9bb596b70103e86c47bc
SergeOin <pierre.logre@epitech.digital>
2021-12-08 18:47:18 +0100

    feat(server.js): node js server

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1c667e6c560efa3c69b9b633ff9e4f74'),
-    },
-  },
-});
