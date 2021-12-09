34afbb2441532c6417e1448647f17ac1bf17262a
SergeOin <pierre.logre@epitech.digital>
2021-12-09 09:21:20 +0100

    fix(server.js): Register et login OK

--- server.js
+++ server.js
@@ -1,64 +1,115 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const Student = require('./model/student')
+const mongoose = require('mongoose')
+const User = require('./model/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const JWT_TOKEN = 'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0'
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
 mongoose.connect('mongodb://localhost:27017/login-app-db')
+
 const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
+app.post('/api/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+
+		const _id = user.id
+
+		const password = await bcrypt.hash(plainTextPassword, 10)
+
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: ';))' })
+	}
+})
+
 app.post('/api/login', async (req, res) => {
-    const { codeine, password } = req.body
-    const student = Student.findOne({ codeine, password }).lean()
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
 
-    if(!user){
-        return res.json({ status: 'error', error: 'Code INE incorrect ou mot de passe incorrect' })      
-    }
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
 
-    if(await bcrypt.compare(password, student.password)){
-        const token = jwt.sign({ id: student._id, codeine: student.codeine }, JWT_TOKEN)
-    }
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
 })
 
 app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
 
-    const { codeine, password: plainTextPassword } = req.body
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
 
-    if(!codeine || typeof codeine !== 'string'){
-        return res.json({ status: 'error', error: 'Code INE faux' })
-    }
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
 
-    if(!plainTextPassword || typeof plainTextPassword !== 'string'){
-        return res.json({ status: 'error', error: 'Mot de passe invalide' })
-    }
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
 
-    if(plainTextPassword.length < 5){
-        return res.json({ status: 'error', error: 'Mot de passe trop petit, minimum 6 caractères' })
-    }
+	const password = await bcrypt.hash(plainTextPassword, 10)
 
-    const password = await bcrypt.hash(plainTextPassword, 12)
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
 
-    try {
-        await Student.create({
-            codeine,
-            password
-        })
-
-    } catch(error) {
-        if(error.code === 11000){
-            return res.json({ status: 'error', error: 'Code INE faux' })
-        }
-        throw error
-    }
-    res.json({ status: 'ok' })
+	res.json({ status: 'ok' })
 })
 
 app.listen(9999, () => {
-    console.log('Serveur OK')
+	console.log('Server up at 9999')
 })
