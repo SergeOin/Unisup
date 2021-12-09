8e72872181b33f91bdf4b509a4f19f1b1e1eafcd
SergeOin <pierre.logre@epitech.digital>
2021-12-09 10:29:17 +0100

    feat(profil.html): Si non connecté pas accès

--- model/user.js
+++ model/user.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
-
 const UserSchema = new mongoose.Schema(
 	{
 		username: { type: String, required: true, unique: true },
-		password: { type: String, required: true }
+		password: { type: String, required: true },
+		prenom: { type: String, required: true },
+		nom: { type: String, required: true }
 	},
 	{ collection: 'users' }
 )
-
 const model = mongoose.model('UserSchema', UserSchema)
-
 module.exports = model
